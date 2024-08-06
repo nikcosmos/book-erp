db02312d9cd784845039a13654246681fe3c41e5
Nik Horbachov <nikcosmos@gmail.com>
2024-08-06 21:09:05 +0300

    feat: add auth store

---
+++ src/shared/store/auth.ts
@@ -0,0 +1,44 @@
+import { defineStore } from 'pinia'
+import { ref, watchEffect, type Ref } from 'vue'
+import { userInfoService } from '../services/auth/api'
+import { type UserInfo } from '../services/auth/model'
+import { useRouter } from 'vue-router'
+import { Route } from '../routes'
+
+interface AuthStore {
+  isAuth: Ref<boolean>
+  userInfo: Ref<UserInfo | null>
+  setIsAuth: (val: boolean) => void
+  signOut: () => void
+}
+
+export const useAuthStore = defineStore('user', (): AuthStore => {
+  const router = useRouter()
+  const isAuth = ref<boolean>(false)
+  const userInfo = ref<UserInfo | null>(null)
+
+  const access = localStorage.getItem('access')
+  if (access) isAuth.value = true
+
+  watchEffect(async () => {
+    if (!userInfo.value && isAuth.value) {
+      try {
+        const res = await userInfoService()
+        return res
+      } catch (error) {
+        console.log('UserInfoError')
+      }
+    }
+  })
+
+  const setIsAuth = (val: boolean) => (isAuth.value = val)
+
+  const signOut = () => {
+    localStorage.removeItem('access')
+    isAuth.value = false
+    userInfo.value = null
+    router.replace({ path: Route.LOGIN })
+  }
+
+  return { isAuth, userInfo, setIsAuth, signOut }
+})
