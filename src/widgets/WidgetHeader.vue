6a5e0ad63b779b74eadf00a6edef3eb584c3f6a6
Nik Horbachov <nikcosmos@gmail.com>
2024-08-06 21:11:02 +0300

    feat: - add sign out to header - add condition to header

--- src/widgets/WidgetHeader.vue
+++ src/widgets/WidgetHeader.vue
@@ -1,16 +1,26 @@
 <script lang="ts" setup>
 import { Route } from '@/shared/routes'
+import { useAuthStore } from '@/shared/store/auth'
 import UiContainer from '@/shared/ui/UiContainer.vue'
+import { storeToRefs } from 'pinia'
 import { RouterLink } from 'vue-router'
+
+const authStore = useAuthStore()
+const { signOut } = authStore
+const { isAuth } = storeToRefs(authStore)
 </script>
 
 <template>
   <header class="header">
     <UiContainer>
-      <div class="header__content">
-        <RouterLink :to="{ path: Route.LOGIN }">Login</RouterLink>
-        <RouterLink :to="{ path: Route.BOOKS }">Books</RouterLink>
-        <RouterLink :to="{ path: Route.USERS }">Users</RouterLink>
+      <div v-if="isAuth" class="header__content">
+        <div class="header__link">
+          <RouterLink :to="{ path: Route.BOOKS }">Books</RouterLink>
+          <RouterLink :to="{ path: Route.USERS }">Users</RouterLink>
+        </div>
+        <div class="header__action">
+          <button type="button" @click="signOut">Sign Out</button>
+        </div>
       </div>
     </UiContainer>
   </header>
@@ -19,9 +29,18 @@ import { RouterLink } from 'vue-router'
 <style lang="scss" scoped>
 .header {
   padding: 20px 0px;
+
   &__content {
     display: flex;
+    justify-content: space-between;
+  }
+
+  &__link {
+    display: flex;
     gap: 12px;
+  }
+
+  &__action {
   }
 }
 </style>
