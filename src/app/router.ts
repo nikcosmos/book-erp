a56c75cb0ee6563b6edffc48033d713513d8405d
Nik Horbachov <nikcosmos@gmail.com>
2024-08-06 21:09:37 +0300

    feat: add route middleware

--- src/app/router.ts
+++ src/app/router.ts
@@ -3,23 +3,40 @@ import PageLogin from '../pages/PageLogin.vue'
 import PageBooks from '../pages/PageBooks.vue'
 import PageUsers from '@/pages/PageUsers.vue'
 import { Route } from '@/shared/routes'
+import { useAuthStore } from '@/shared/store/auth'
+import { storeToRefs } from 'pinia'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: Route.LOGIN,
-      component: PageLogin
+      component: PageLogin,
+      meta: { authPage: true }
     },
     {
       path: Route.BOOKS,
-      component: PageBooks
+      component: PageBooks,
+      meta: { requiresAuth: true }
     },
     {
       path: Route.USERS,
-      component: PageUsers
+      component: PageUsers,
+      meta: { requiresAuth: true }
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  const { isAuth } = storeToRefs(authStore)
+
+  if (to.matched.some((record) => record.meta.authPage)) {
+    isAuth.value ? next({ path: Route.BOOKS }) : next()
+  }
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    isAuth.value ? next() : next({ path: Route.LOGIN })
+  }
 })
 
 export default router
