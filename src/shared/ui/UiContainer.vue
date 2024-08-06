a2c4e9e8e07f8f6666a04a39b797474c21b2a971
Nik Horbachov <nikcosmos@gmail.com>
2024-08-06 11:34:46 +0300

    feat: add container

---
+++ src/shared/ui/UiContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1130px;
+  margin: auto;
+  padding: 0px 15px;
+}
+</style>
