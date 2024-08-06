87167aad38c2ec31ef445c719d3c77f0f0e110cd
Nik Horbachov <nikcosmos@gmail.com>
2024-08-06 11:41:38 +0300

    chore: - add link to header - update null

--- src/widgets/WidgetHeader.vue
+++ src/widgets/WidgetHeader.vue
@@ -1,11 +1,27 @@
 <script lang="ts" setup>
+import { ROUTE } from '@/shared/routes'
 import UiContainer from '@/shared/ui/UiContainer.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <UiContainer> Header </UiContainer>
+  <header class="header">
+    <UiContainer>
+      <div class="header__content">
+        <RouterLink :to="{ path: ROUTE.LOGIN }">Login</RouterLink>
+        <RouterLink :to="{ path: ROUTE.BOOKS }">Books</RouterLink>
+        <RouterLink :to="{ path: ROUTE.USERS }">Users</RouterLink>
+      </div>
+    </UiContainer>
   </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header {
+  padding: 20px 0px;
+  &__content {
+    display: flex;
+    gap: 12px;
+  }
+}
+</style>
