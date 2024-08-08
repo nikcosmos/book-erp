3a115294a22cd9306c9be24b223bcaff00a0ba98
Nik Horbachov <nikcosmos@gmail.com>
2024-08-08 15:55:57 +0300

    chore: add custom button & input

---
+++ src/shared/ui/UIButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.button {
+  padding: 8px;
+  background: var(--primary);
+  border-radius: 8px;
+  transition: opacity 0.2s ease 0s;
+  color: var(--white);
+  @media (any-hover: hover) {
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+  &:active {
+    opacity: 0.6;
+  }
+}
+</style>
---
+++ src/shared/ui/UIInput.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const model = defineModel()
+</script>
+
+<template>
+  <input class="input" v-model="model" />
+</template>
+
+<style lang="scss" scoped>
+.input {
+  border: 2px solid var(--primary);
+  border-radius: 8px;
+  padding: 8px;
+  font-size: 16px;
+  color: var(--white);
+  background: var(--black);
+  &:focus-within {
+    box-shadow: 0 0 3px var(--primary);
+  }
+}
+</style>
