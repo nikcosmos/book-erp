460675909d192463df760b7e6765e20d53de96a6
Nik Horbachov <nikcosmos@gmail.com>
2024-08-08 15:56:11 +0300

    chore: udpate login form

--- src/feature/auth/ui/LoginForm.vue
+++ src/feature/auth/ui/LoginForm.vue
@@ -2,6 +2,8 @@
 import { Route } from '@/shared/routes'
 import { loginService } from '@/shared/services/auth/api'
 import { useAuthStore } from '@/shared/store/auth'
+import UIButton from '@/shared/ui/UIButton.vue'
+import UIInput from '@/shared/ui/UIInput.vue'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
@@ -10,12 +12,12 @@ const router = useRouter()
 const login = ref<string>('admin')
 const password = ref<string>('admin')
 
-const { setIsAuth } = useAuthStore()
+const authStore = useAuthStore()
 async function submitLogin() {
   try {
     const res = await loginService(login.value, password.value)
     localStorage.setItem('access', res.access)
-    setIsAuth(true)
+    authStore.setIsAuth(true)
     router.push(Route.BOOKS)
   } catch (error) {
     console.log('Login Error')
@@ -24,24 +26,30 @@ async function submitLogin() {
 </script>
 
 <template>
-  <form class="login-form" @submit.prevent="submitLogin">
-    <input class="login-form__input" type="text" name="login" v-model="login" />
-    <input class="login-form__input" type="password" name="password" v-model="password" />
-    <button class="login-form__button" type="submit">Login</button>
-  </form>
+  <div class="login">
+    <h1 class="login__title">Login</h1>
+    <form class="login__form" @submit.prevent="submitLogin">
+      <UIInput type="text" name="login" v-model="login" />
+      <UIInput type="password" name="password" v-model="password" />
+      <UIButton type="submit">Enter</UIButton>
+    </form>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.login-form {
-  display: flex;
-  flex-direction: column;
-  gap: 12px;
+.login {
   max-width: 500px;
   margin: auto;
-  &__input {
+  &__title {
+    font-size: 20px;
+    margin-bottom: 16px;
+    text-align: center;
   }
 
-  &__button {
+  &__form {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
   }
 }
 </style>
