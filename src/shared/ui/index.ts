0786aa7de4d939cf9b02109e5684110f9e8a07fc
Nik Horbachov <nikcosmos@gmail.com>
2024-08-08 16:03:45 +0300

    chroe: add UI re-export

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,4 @@
+export { default as UIButton } from './UIButton.vue'
+export { default as UIInput } from './UIInput.vue'
+export { default as UIModal } from './UIModal.vue'
+export { default as UiContainer } from './UiContainer.vue'
